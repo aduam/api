7b85819477e4c9b630c4e7aa0794115e55047868
aduam <alanduam@gmail.com>
2020-06-24 15:58:53 -0600

    Added cors to app

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 require('dotenv').config()
 const express = require('express');
 const fileupload = require('express-fileupload');
+const cors = require('cors')
 const route = require('./router')
 
 const app = express();
 const PORT = process.env.PORT || 4000
 
+const corsOptions = {
+  origin: 'http://localhost:3001',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 //middlewares
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(fileupload());
 app.use(route)
