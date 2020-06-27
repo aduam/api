516b9c66f7aa44651c105ecf885af5ebf578b06a
aduam <alanduam@gmail.com>
2020-06-27 16:13:08 -0600

    added cors in prod

--- index.js
+++ index.js
@@ -7,17 +7,23 @@ const route = require('./router')
 const app = express();
 const PORT = process.env.PORT || 4000
 
-const corsOptions = {
-  origin: 'http://localhost:3001',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'https://front-transport.vercel.app/']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
 }
 
 //middlewares
-app.use(cors(corsOptions))
+app.use(cors(corsOptionsDelegate))
 app.use(express.json())
 app.use(fileupload());
 app.use(route)
 
 app.listen(PORT, async () => {
-  console.log(`server listening in http://localhost:${PORT}`)
+  console.log(`server listening in port ${PORT}`)
 })
